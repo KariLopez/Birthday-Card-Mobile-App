bcf61178796ac440fe1d72fca482b2316cb1bc63
klopez91 <31086446+klopez91@users.noreply.github.com>
2018-01-23 20:49:35 -0500

    background image and positioning

--- app/src/main/java/com/example/klopez/happybirthday/MainActivity.java
+++ app/src/main/java/com/example/klopez/happybirthday/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.klopez.happybirthday;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
